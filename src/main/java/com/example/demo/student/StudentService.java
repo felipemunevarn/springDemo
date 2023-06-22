f7fc5de745ea9d6b287177af5a344d7bae09cb97
feliMune <felipemunevarn@gmail.com>
2023-06-22 16:17:44 -0300

    adding delete and update students

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,9 +3,12 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service    
 public class StudentService {
@@ -22,7 +25,31 @@ public class StudentService {
 	}
 
     public void addNewStudent(Student student) {
-		System.out.println(student);
+		Optional<Student> studentOptional = 
+			studentRepository.findStudentByEmail(student.getEmail());
+		if (studentOptional.isPresent()){
+			throw new IllegalStateException("email taken");
+		}
+		studentRepository.save(student);
+	}
+
+	public void deleteStudent(Long id) {
+		if (!studentRepository.existsById(id)){
+			throw new IllegalStateException(
+				"student with id " + id + " does not exist");
+		}
+		studentRepository.deleteById(id);
+	}
+
+	@Transactional
+	public void updateStudent(Long id, Student student) {
+		Optional<Student> studentOptional = studentRepository.findById(id);
+		if (!studentOptional.isPresent()){
+			throw new IllegalStateException("student with id " + id + " does not exist");
+		}
+		studentOptional.get().setName(student.getName());
+		studentOptional.get().setEmail(student.getEmail());
+		studentRepository.save(studentOptional.get());
 	}
 
 }
